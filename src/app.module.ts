6029fb10f43324003b23c1b3ad832339d4887bce
Andre0n <andreon098@gmail.com>
2024-08-24 11:18:51 -0300

    feat: add AccountModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AccountModule } from './modules/account/account.module';
 
 @Module({
-  imports: [],
+  imports: [AccountModule],
 })
 export class AppModule {}
