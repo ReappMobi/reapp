8dff093310db937b0cd014ae0d5996acec5a087c
Andre0n <andreon098@gmail.com>
2024-08-29 10:34:54 -0300

    feat(run-test.sh): update script to use local npm installation

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 
 # Prepare the environment
-dotenvx run --env-file ./.env.test.local -- npx prisma migrate reset --force
+npx dotenvx run --env-file ./.env.test.local -- npx prisma migrate reset --force
 
 # Run the tests
-dotenvx run --env-file ./.env.test.local -- npm run test # Unit tests
-dotenvx run --env-file ./.env.test.local -- npm run test:e2e # E2E tests
+npx dotenvx run --env-file ./.env.test.local -- npm run test # Unit tests
+npx dotenvx run --env-file ./.env.test.local -- npm run test:e2e # E2E tests
