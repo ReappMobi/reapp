8f6c783d11472494fdb3f659e9666ef69bda5a72
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-09-08 14:28:59 -0300

    feat: add AuthenticationModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AccountModule } from './modules/account/account.module';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
 
 @Module({
-  imports: [AccountModule],
+  imports: [AccountModule, AuthenticationModule],
 })
 export class AppModule {}
