15215a42e58a12c16b08b70eaeaab586e668b18c
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-09-08 14:35:42 -0300

    feat: add authentication module with controller, service and prisma integration

---
+++ src/modules/authentication/authentication.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Post, Body, HttpStatus } from '@nestjs/common';
+import { AuthService } from './authentication.service';
+import { LoginDto } from './dto/login.dto';
+import { LoginGoogleDto } from './dto/loginGoogle.dto';
+import { HttpException } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthenticationController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    try {
+      return await this.authService.login(loginDto);
+    } catch (error) {
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  @Post('login-google')
+  async loginWithGoogle(@Body() loginGoogleDto: LoginGoogleDto) {
+    try {
+      return await this.authService.loginWithGoogle(loginGoogleDto);
+    } catch (error) {
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
