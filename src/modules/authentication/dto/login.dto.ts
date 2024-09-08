91dab19b7f489d9151da548a7032f58ba837d0d8
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-09-08 14:36:29 -0300

    feat: add authentication dto

---
+++ src/modules/authentication/dto/login.dto.ts
@@ -0,0 +1,14 @@
+import { Expose } from 'class-transformer';
+
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class LoginDto {
+  @Expose()
+  @IsEmail({}, { message: 'O formato de email é inválido.' })
+  @IsNotEmpty({ message: 'O email é obrigatório.' })
+  email: string;
+
+  @Expose()
+  @IsNotEmpty({ message: 'A senha é obrigatória.' })
+  password: string;
+}
