33f525f29186ac15af0b99accec780605af4dbaa
Andre0n <andreon098@gmail.com>
2024-09-10 21:56:21 -0300

    feat: add DonationController for handling donation requests and callbacks

---
+++ src/modules/donation/donation.controller.ts
@@ -0,0 +1,19 @@
+import { Controller } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { RequestDonationDto } from './dto/request-donation.dto';
+import { DonationService } from './donation.service';
+
+@Controller('donation')
+export class DonationController {
+  constructor(private readonly donationService: DonationService) {}
+
+  @Post('request')
+  requestDonation(requestDonationDto: RequestDonationDto) {
+    return this.donationService.requestDonation(requestDonationDto);
+  }
+
+  @Post('callback')
+  callbackDonation() {
+    return this.donationService.callbackDonation();
+  }
+}
