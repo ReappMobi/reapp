15fee9c2e49c08acde8bf49a945b6b6a10487c11
Andre0n <andreon098@gmail.com>
2024-09-10 21:59:12 -0300

    feat(app.module): add `DonationModule` and  `MercadopagoService`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccountModule } from './modules/account/account.module';
 import { AuthenticationModule } from './modules/authentication/authentication.module';
+import { DonationModule } from './modules/donation/donation.module';
+import { MercadopagoService } from './services/mercadopago/mercadopago.service';
 
 @Module({
-  imports: [AccountModule, AuthenticationModule],
+  imports: [AccountModule, AuthenticationModule, DonationModule],
+  providers: [MercadopagoService],
 })
 export class AppModule {}
