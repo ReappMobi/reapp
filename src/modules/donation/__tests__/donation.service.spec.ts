b7ca244ccb731d5e4d1d30b80c52fc781528250a
Andre0n <andreon098@gmail.com>
2024-09-13 23:24:31 -0300

    refactor: improve DonationService code clarity and consistency
    - Fix negative donation amount validation
    - Add name and email fields to RequestDonationDto
    - Update buildRequestBody method to use requestDonationDto
    - Update buildRequestBody method in DonationService to accept bodyInfo parameter
    - Update donation service to use mercadopagoRequest type

--- src/modules/donation/__tests__/donation.service.spec.ts
+++ src/modules/donation/__tests__/donation.service.spec.ts
@@ -4,11 +4,10 @@ import { MercadopagoService } from '../../../services/mercadopago/mercadopago.se
 import { PrismaService } from '../../../database/prisma.service';
 import { RequestDonationDto } from '../dto/request-donation.dto';
 
-describe('DonationService', () => {
+describe('DonationService tests', () => {
   let service: DonationService;
   let mercadopagoService: MercadopagoService;
   let prismaService: PrismaService;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -28,6 +27,9 @@ describe('DonationService', () => {
             institution: {
               findUnique: jest.fn(),
             },
+            account: {
+              findUnique: jest.fn(),
+            },
           },
         },
       ],
@@ -42,192 +44,201 @@ describe('DonationService', () => {
     expect(service).toBeDefined();
   });
 
-  describe('requestDonation', () => {
-    it('should throw an error if amount is negative', async () => {
-      const requestDonationDto: RequestDonationDto = {
-        userToken: 'test',
-        amount: -10,
-        institutionId: 1,
-        projectId: 1,
-        description: 'test',
-      };
+  describe('error case', () => {
+    const requestDonationDto: RequestDonationDto = {
+      name: 'test',
+      email: 'test@test.com',
+      userToken: 'test',
+      amount: 10,
+      institutionId: 1,
+      projectId: 1,
+      description: 'test',
+    };
+    it('should throw an error if the institution does not exist', async () => {
+      (prismaService.institution.findUnique as jest.Mock).mockResolvedValue(
+        null,
+      );
+      (prismaService.account.findUnique as jest.Mock).mockResolvedValue({
+        email: 'test@test.com',
+        name: 'test',
+      });
+      requestDonationDto.projectId = null;
+      await expect(service.requestDonation(requestDonationDto)).rejects.toThrow(
+        'Instituição não encontrada',
+      );
+    });
+
+    it('should throw an error if the project does not exist', async () => {
+      (prismaService.account.findUnique as jest.Mock).mockResolvedValue({
+        email: 'test@test.com',
+        name: 'test',
+      });
+      (prismaService.project.findUnique as jest.Mock).mockResolvedValue(null);
+      requestDonationDto.institutionId = null;
+      requestDonationDto.projectId = 1;
+      await expect(service.requestDonation(requestDonationDto)).rejects.toThrow(
+        'Projeto não encontrado',
+      );
+    });
+
+    it('should throw an error if the amount is less than or equal to 0', async () => {
+      requestDonationDto.amount = 0;
+      (prismaService.account.findUnique as jest.Mock).mockResolvedValue({
+        email: 'test@test.com',
+        name: 'test',
+      });
       await expect(service.requestDonation(requestDonationDto)).rejects.toThrow(
         'A quantidade de doação não pode ser negativa',
       );
     });
 
-    it('should throw an error if amount is zero', async () => {
-      const requestDonationDto: RequestDonationDto = {
-        userToken: 'test',
-        amount: 0,
-        institutionId: 1,
-        projectId: 1,
-        description: 'test',
-      };
-      await expect(service.requestDonation(requestDonationDto)).rejects.toThrow(
-        'A quantidade de doação não pode ser negativa',
-      );
-    });
+    it('should throw an error if the amount is less than 0.01', async () => {
+      requestDonationDto.amount = 0.001;
+      (prismaService.account.findUnique as jest.Mock).mockResolvedValue({
+        email: 'test@test.com',
+        name: 'test',
+      });
 
-    it('should throw an error if amount is less than 0.01', async () => {
-      const requestDonationDto: RequestDonationDto = {
-        userToken: 'test',
-        amount: 0.001,
-        institutionId: 1,
-        projectId: 1,
-        description: 'test',
-      };
       await expect(service.requestDonation(requestDonationDto)).rejects.toThrow(
         'Valor inválido',
       );
     });
 
-    describe('requestProjectDonation', () => {
-      it('should call mercadopagoService.processPayment with correct data', async () => {
-        const requestDonationDto: RequestDonationDto = {
-          institutionId: null,
-          userToken: 'test',
-          amount: 10,
-          projectId: 1,
-          description: 'test',
-        };
-        const project = {
+    it('should throw an error if account is not exists', async () => {
+      (prismaService.account.findUnique as jest.Mock).mockResolvedValue(null);
+      await expect(service.requestDonation(requestDonationDto)).rejects.toThrow(
+        'Usuário não encontrado',
+      );
+    });
+
+    it('should throw an error if account is an institution', async () => {
+      (prismaService.account.findUnique as jest.Mock).mockResolvedValue({
+        email: 'test@test.com',
+        name: 'test',
+        institution: {
           id: 1,
-          name: 'test project',
-        };
-        (prismaService.project.findUnique as jest.Mock).mockResolvedValue(
-          project,
-        );
-        (mercadopagoService.processPayment as jest.Mock).mockResolvedValue(
-          'payment link',
-        );
-        await service.requestDonation(requestDonationDto);
-        expect(mercadopagoService.processPayment).toHaveBeenCalledWith({
-          items: [
-            {
-              id: 'not_implemented',
-              title: project.name,
-              description: requestDonationDto.description,
-              quantity: 1,
-              currency_id: 'BRL',
-              unit_price: requestDonationDto.amount,
-            },
-          ],
-          payer: {
-            name: 'not_implemented',
-            email: 'not_implemented',
-          },
-          notification_url: 'localhost:3000/donation/notify',
-          external_reference: 'not_implemented',
-        });
+        },
       });
-      it('should throw an error if project is not found', async () => {
-        const requestDonationDto: RequestDonationDto = {
-          institutionId: null,
-          userToken: 'test',
-          amount: 10,
-          projectId: 1,
-          description: 'test',
-        };
-        (prismaService.project.findUnique as jest.Mock).mockResolvedValue(null);
-        await expect(
-          service.requestDonation(requestDonationDto),
-        ).rejects.toThrow('Projeto não encontrado');
+      await expect(service.requestDonation(requestDonationDto)).rejects.toThrow(
+        'Usuário é uma instituição e não pode fazer doações',
+      );
+    });
+  });
+
+  describe('success case', () => {
+    const requestDonationDto: RequestDonationDto = {
+      name: 'test',
+      email: 'test@test.com',
+      userToken: 'test',
+      amount: 10,
+      institutionId: 1,
+      projectId: 1,
+      description: 'test',
+    };
+
+    it('should call mercadopagoService.processPayment with correct data when donate to a project', async () => {
+      (prismaService.project.findUnique as jest.Mock).mockResolvedValue({
+        name: 'test',
+      });
+      (prismaService.account.findUnique as jest.Mock).mockResolvedValue({
+        email: 'test@test.com',
+        name: 'test',
+      });
+      (mercadopagoService.processPayment as jest.Mock).mockResolvedValue(
+        'payment link',
+      );
+      await service.requestDonation(requestDonationDto);
+      expect(mercadopagoService.processPayment).toHaveBeenCalledWith({
+        items: [
+          {
+            id: 'not_implemented',
+            title: 'test',
+            description: requestDonationDto.description,
+            quantity: 1,
+            currency_id: 'BRL',
+            unit_price: requestDonationDto.amount,
+          },
+        ],
+        payer: {
+          name: requestDonationDto.name,
+          email: requestDonationDto.email,
+        },
+        notification_url: 'localhost:3000/donation/notify',
+        external_reference: 'not_implemented',
       });
     });
 
-    describe('requestInstitutionDonation', () => {
-      it('should call mercadopagoService.processPayment with correct data', async () => {
-        const requestDonationDto: RequestDonationDto = {
-          userToken: 'test',
-          amount: 10,
-          institutionId: 1,
-          description: 'test',
-        };
-        const institution = {
-          id: 1,
-          account: {
-            name: 'test institution',
-          },
-        };
-        (prismaService.institution.findUnique as jest.Mock).mockResolvedValue(
-          institution,
-        );
-        (mercadopagoService.processPayment as jest.Mock).mockResolvedValue(
-          'payment link',
-        );
-        await service.requestDonation(requestDonationDto);
-        expect(mercadopagoService.processPayment).toHaveBeenCalledWith({
-          items: [
-            {
-              id: 'not_implemented',
-              title: institution.account.name,
-              description: requestDonationDto.description,
-              quantity: 1,
-              currency_id: 'BRL',
-              unit_price: requestDonationDto.amount,
-            },
-          ],
-          payer: {
-            name: 'not_implemented',
-            email: 'not_implemented',
-          },
-          notification_url: 'localhost:3000/donation/notify',
-          external_reference: 'not_implemented',
-        });
+    it('should call mercadopagoService.processPayment with correct data when donate to a institution', async () => {
+      (prismaService.institution.findUnique as jest.Mock).mockResolvedValue({
+        account: {
+          name: 'test',
+        },
       });
-
-      it('should throw an error if institution is not found', async () => {
-        const requestDonationDto: RequestDonationDto = {
-          userToken: 'test',
-          amount: 10,
-          institutionId: 1,
-          description: 'test',
-        };
-        (prismaService.institution.findUnique as jest.Mock).mockResolvedValue(
-          null,
-        );
-        await expect(
-          service.requestDonation(requestDonationDto),
-        ).rejects.toThrowError('Instituição não encontrada');
+      (prismaService.account.findUnique as jest.Mock).mockResolvedValue({
+        email: 'test@test.com',
+        name: 'test',
+      });
+      (mercadopagoService.processPayment as jest.Mock).mockResolvedValue(
+        'payment link',
+      );
+      requestDonationDto.projectId = null;
+      requestDonationDto.institutionId = 1;
+      await service.requestDonation(requestDonationDto);
+      expect(mercadopagoService.processPayment).toHaveBeenCalledWith({
+        items: [
+          {
+            id: 'not_implemented',
+            title: 'test',
+            description: requestDonationDto.description,
+            quantity: 1,
+            currency_id: 'BRL',
+            unit_price: requestDonationDto.amount,
+          },
+        ],
+        payer: {
+          name: requestDonationDto.name,
+          email: requestDonationDto.email,
+        },
+        notification_url: 'localhost:3000/donation/notify',
+        external_reference: 'not_implemented',
       });
     });
-    describe('requestGeneralDonation', () => {
-      it('should call mercadopagoService.processPayment with correct data', async () => {
-        const requestDonationDto: RequestDonationDto = {
-          institutionId: null,
-          userToken: 'test',
-          amount: 10,
-          description: 'test',
-        };
-        (mercadopagoService.processPayment as jest.Mock).mockResolvedValue(
-          'payment link',
-        );
-        await service.requestDonation(requestDonationDto);
-        expect(mercadopagoService.processPayment).toHaveBeenCalledWith({
-          items: [
-            {
-              id: 'not_implemented',
-              title: 'Reapp',
-              description: requestDonationDto.description,
-              quantity: 1,
-              currency_id: 'BRL',
-              unit_price: requestDonationDto.amount,
-            },
-          ],
-          payer: {
-            name: 'not_implemented',
-            email: 'not_implemented',
+
+    it('should call mercadopagoService.processPayment with correct data with a general donation', async () => {
+      (prismaService.account.findUnique as jest.Mock).mockResolvedValue({
+        email: 'test@test.com',
+        name: 'test',
+      });
+      (mercadopagoService.processPayment as jest.Mock).mockResolvedValue(
+        'payment link',
+      );
+      requestDonationDto.projectId = null;
+      requestDonationDto.institutionId = null;
+
+      await service.requestDonation(requestDonationDto);
+      expect(mercadopagoService.processPayment).toHaveBeenCalledWith({
+        items: [
+          {
+            id: 'not_implemented',
+            title: 'Reapp',
+            description: requestDonationDto.description,
+            quantity: 1,
+            currency_id: 'BRL',
+            unit_price: requestDonationDto.amount,
           },
-          notification_url: 'localhost:3000/donation/notify',
-          external_reference: 'not_implemented',
-        });
+        ],
+        payer: {
+          name: requestDonationDto.name,
+          email: requestDonationDto.email,
+        },
+        notification_url: 'localhost:3000/donation/notify',
+        external_reference: 'not_implemented',
       });
     });
   });
 
-  describe('callbackDonation', () => {
-    it('should return "Callback donation"', async () => {
+  describe('notifyDonation', () => {
+    it('should return "notify donation"', async () => {
       const result = await service.notifyDonation();
       expect(result).toBe('notify donation');
     });
