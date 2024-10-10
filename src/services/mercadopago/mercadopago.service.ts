f100e6351810b89ab5463ffec884e6df2cc6c256
Andre0n <andreon098@gmail.com>
2024-10-09 21:51:13 -0300

    feat(mercadopago.service): change `Payment` to `Preference`

--- src/services/mercadopago/mercadopago.service.ts
+++ src/services/mercadopago/mercadopago.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { MercadoPagoConfig, Payment } from 'mercadopago';
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+
 
 @Injectable()
 export class MercadopagoService {
@@ -16,22 +17,22 @@ export class MercadopagoService {
   }
 
   async processPayment(data: any): Promise<any> {
-    const payment = new Payment(this.client);
+    const preference = new Preference(this.client);
     const requestOptions = {
       idempotencyKey: process.env.MERCADOPAGO_IDEMPOTENCY_KEY,
     };
     try {
-      const response = await payment.create({ body: data, requestOptions });
+      const response = await preference.create({ body: data, requestOptions });
       return response;
     } catch (error) {
       return error;
     }
   }
 
-  async getPayment(paymentId: string): Promise<any> {
-    const payment = new Payment(this.client);
+  async getPayment(preferenceId: string): Promise<any> {
+    const preference = new Preference(this.client);
     try {
-      const response = await payment.get({ id: paymentId });
+      const response = await preference.get({ preferenceId });
       return response;
     } catch (error) {
       return error;
