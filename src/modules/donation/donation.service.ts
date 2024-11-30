5ddf696c791ff808ecced77e1e95a32c18fd8aa7
Andre0n <andreon098@gmail.com>
2024-11-30 11:42:23 -0300

    feat: fix limit and page type

--- src/modules/donation/donation.service.ts
+++ src/modules/donation/donation.service.ts
@@ -194,7 +194,7 @@ export class DonationService {
   async getAllDonations(page: number, limit: number) {
     const donations = await this.prismaService.donation.findMany({
       skip: (page - 1) * limit,
-      take: limit,
+      take: Number(limit),
     });
     return donations;
   }
@@ -204,24 +204,26 @@ export class DonationService {
     page: number,
     limit: number,
   ) {
-    const donations = await this.prismaService.donation.findMany({
-      where: {
-        institutionId,
-      },
-      skip: (page - 1) * limit,
-      take: limit,
-    });
+    const donations =
+      (await this.prismaService.donation.findMany({
+        where: {
+          institutionId: Number(institutionId),
+        },
+        skip: (page - 1) * limit,
+        take: Number(limit),
+      })) || [];
     return donations;
   }
 
   async getDonationsByProject(projectId: number, page: number, limit: number) {
-    const donations = await this.prismaService.donation.findMany({
-      where: {
-        projectId,
-      },
-      skip: (page - 1) * limit,
-      take: limit,
-    });
+    const donations =
+      (await this.prismaService.donation.findMany({
+        where: {
+          projectId: Number(projectId),
+        },
+        skip: (page - 1) * limit,
+        take: Number(limit),
+      })) || [];
     return donations;
   }
 
@@ -232,15 +234,17 @@ export class DonationService {
     institutionId: number = null,
     projectId: number = null,
   ) {
-    const donations = await this.prismaService.donation.findMany({
-      where: {
-        donorId,
-        projectId: projectId ? projectId : undefined,
-        institutionId: institutionId ? institutionId : undefined,
-      },
-      skip: (page - 1) * limit,
-      take: limit,
-    });
+    const donations =
+      (await this.prismaService.donation.findMany({
+        where: {
+          donorId: Number(donorId),
+          projectId: projectId ? projectId : undefined,
+          institutionId: institutionId ? institutionId : undefined,
+        },
+        skip: (page - 1) * limit,
+        take: Number(limit),
+      })) || [];
+
     return donations;
   }
 
