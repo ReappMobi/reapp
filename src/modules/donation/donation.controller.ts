84670481be73e3c30505799dda6efdd05ffebcaa
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2025-01-07 21:31:41 -0300

    refactor: update requestDonation controller to add Body decorator

--- src/modules/donation/donation.controller.ts
+++ src/modules/donation/donation.controller.ts
@@ -1,18 +1,39 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { Post } from '@nestjs/common';
 import { RequestDonationDto } from './dto/request-donation.dto';
 import { DonationService } from './donation.service';
 import { NotificationRequestDto } from './dto/notification.dto';
+import { AuthGuard } from '../auth/auth.guard';
+
+interface RequestWithUser extends Request {
+  user?: { id: number };
+}
 
 @Controller('donation')
 export class DonationController {
   constructor(private readonly donationService: DonationService) {}
 
   @Post('request')
-  requestDonation(requestDonationDto: RequestDonationDto) {
-    return this.donationService.requestDonation(requestDonationDto);
+  @UseGuards(AuthGuard)
+  requestDonation(
+    @Body() requestDonationDto: RequestDonationDto,
+    @Req() req: RequestWithUser,
+  ) {
+    const accountId = req.user.id;
+    return this.donationService.requestDonation(
+      requestDonationDto,
+      Number(accountId),
+    );
   }
-
+  x;
   @Get('all')
   findAll(@Query('page') page: number = 1, @Query('limit') limit: number = 10) {
     return this.donationService.getAllDonations(page, limit);
