fc994a9c24faf3a358557935785f59e8162ae0c4
Andre0n <andreon098@gmail.com>
2025-01-18 11:48:26 -0300

    feat: update `auth.guard` error messages

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -16,7 +16,7 @@ export class AuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Nenhum token fornecido');
     }
     try {
       const payload = await this.jwtService.verifyAsync(token, {
@@ -24,7 +24,7 @@ export class AuthGuard implements CanActivate {
       });
       request['user'] = payload;
     } catch {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('O token fornecido é inválido');
     }
     return true;
   }
