d399603b78eff7622e1eb37f42e6a9064b9aa0a8
André Gabriel <andreon098@gmail.com>
2025-03-02 17:33:15 -0300

    feat: add account seeding script with donor and institution accounts

---
+++ seeds/account.seed.ts
@@ -0,0 +1,58 @@
+import { PrismaClient } from "@prisma/client";
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+const defaultPassword = 'pass1234';
+
+async function main() {
+  const passwordHash = await bcrypt.hash(defaultPassword, 10);
+  await prisma.account.create({
+    data: {
+      email: 'donor@reapp.com',
+      passwordHash,
+      name: 'Donor',
+      note: 'Donor account',
+      status: 'ACTIVE',
+      donor: {
+        create: {},
+      },
+    },
+  })
+
+  const category = await prisma.category.create({
+    data: {
+      name: 'Test',
+    },
+  })
+
+  await prisma.account.create({
+    data: {
+      email: 'institution@reapp.com',
+      passwordHash,
+      name: 'Institution',
+      note: 'Institution account',
+      status: 'ACTIVE',
+      institution: {
+        create: {
+          category: {
+            connect: {
+              id: category.id,
+            },
+          },
+          cnpj: '12345678901234',
+          phone: '989999999',
+        },
+      },
+    },
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
