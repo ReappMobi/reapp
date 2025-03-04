b590553d5361feb23c43a21096c696cfd94b8ab2
André Gabriel <andreon098@gmail.com>
2025-03-04 12:31:06 -0300

    feat: add utility functions for mocking admin and user authentication in tests

---
+++ test/utils/index.ts
@@ -0,0 +1,53 @@
+import { INestApplication } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+import * as bcrypt from 'bcrypt'
+import * as request from 'supertest'
+
+const createAdmin = async () => {
+  const prisma = new PrismaClient()
+
+  const account = await prisma.account.findUnique({
+    where: {
+      email: 'admin@reapp.com',
+    },
+  })
+
+  if (account) {
+    return
+  }
+
+  await prisma.account.create({
+    data: {
+      name: 'Admin',
+      email: 'admin@reapp.com',
+      passwordHash: bcrypt.hashSync('pass1234', 10),
+      accountType: 'ADMIN',
+    },
+  })
+}
+
+export const mockAdmin = async (app: INestApplication) => {
+  await createAdmin()
+
+  const response = await request(app.getHttpServer()).post('/auth/login').send({
+    email: 'admin@reapp.com',
+    password: 'pass1234',
+  })
+  return response.body
+}
+
+export const mockInstitution = async (app: INestApplication) => {
+  const response = await request(app.getHttpServer()).post('/auth/login').send({
+    email: 'institution@reapp.com',
+    password: 'pass1234',
+  })
+  return response.body
+}
+
+export const mockDonor = async (app: INestApplication) => {
+  const response = await request(app.getHttpServer()).post('/auth/login').send({
+    email: 'donor@reapp.com',
+    password: 'pass1234',
+  })
+  return response.body
+}
