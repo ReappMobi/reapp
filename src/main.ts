4bb362d7c7b30c9a1cc32689f849006daaf62619
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2025-04-29 16:17:13 -0300

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import * as path from 'path'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: 'http://localhost:5174',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  })
   app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000)
