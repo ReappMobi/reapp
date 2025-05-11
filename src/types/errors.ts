bd23484c8e8c98375de0693372916a3d678dead6
André Gabriel <57845119+Andre0n@users.noreply.github.com>
2025-05-11 10:53:32 -0300

    Add custom exception (#49)
    * feat: add backend error handling and custom exception class
    
    * fix: replace UnauthorizedException with ReappException in AuthGuard

---
+++ src/types/errors.ts
@@ -0,0 +1,41 @@
+export interface BackendBaseError {
+  status: number
+  description: string
+  data: string[]
+}
+
+export enum BackendErrorCodes {
+  INTERNAL_SERVER_ERROR = 'INTERNAL_SERVER_ERROR',
+
+  USER_NOT_FOUND_ERROR = 'USER_NOT_FOUND_ERROR',
+  INVALID_TOKEN_ERROR = 'INVALID_TOKEN_ERROR',
+  USER_NOT_AUTHORIZED_ERROR = 'USER_NOT_AUTHORIZED_ERROR',
+}
+
+const GenericErrors = {
+  [BackendErrorCodes.INTERNAL_SERVER_ERROR]: {
+    status: 500,
+    description: 'Erro inter do servidor',
+    data: [],
+  },
+}
+
+const AuthErrors = {
+  [BackendErrorCodes.USER_NOT_FOUND_ERROR]: {
+    status: 500,
+    description: 'Erro interno do servidor',
+    data: [],
+  },
+  [BackendErrorCodes.INVALID_TOKEN_ERROR]: {
+    status: 401,
+    description: 'O token fornecido está inválido ou já expirou',
+    data: ['invitationToken'],
+  },
+  [BackendErrorCodes.USER_NOT_AUTHORIZED_ERROR]: {
+    status: 401,
+    description: 'O usuário não tem permissão para acessar este recurso',
+    data: [],
+  },
+}
+
+export const BackendErrors = { ...GenericErrors, ...AuthErrors }
