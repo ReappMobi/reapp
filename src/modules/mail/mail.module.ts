5e19b805ea7c894de1f0da4bb4306d22acc0a84a
André Gabriel <andreon098@gmail.com>
2025-10-19 15:24:09 -0300

    Revert "refactor: migrate MailerModule to use ConfigService for settings"
    This reverts commit 9f64de8f1e6d7e68a3da96eb4bd469dca3363eee.

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -2,35 +2,30 @@ import * as path from 'node:path'
 import { MailerModule } from '@nestjs-modules/mailer'
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter'
 import { Module } from '@nestjs/common'
-import { ConfigModule, ConfigService } from '@nestjs/config'
 import { MailService } from './mail.service'
 
 @Module({
   imports: [
-    MailerModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        transport: {
-          host: configService.get('EMAIL_HOST'),
-          port: configService.get('EMAIL_PORT'),
-          secure: false,
-          auth: {
-            user: configService.get('EMAIL_USER'),
-            pass: configService.get('EMAIL_PASSWORD'),
-          },
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: parseInt(process.env.MAIL_PORT),
+        secure: false,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
         },
-        defaults: {
-          from: configService.get('EMAIL_FROM'),
+      },
+      defaults: {
+        from: process.env.MAIL_FROM,
+      },
+      template: {
+        dir: path.join(process.cwd(), 'dist', 'modules', 'mail', 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
         },
-        template: {
-          dir: path.join(process.cwd(), 'dist', 'modules', 'mail', 'templates'),
-          adapter: new HandlebarsAdapter(),
-          options: {
-            strict: true,
-          },
-        },
-      }),
+      },
     }),
   ],
   providers: [MailService],
