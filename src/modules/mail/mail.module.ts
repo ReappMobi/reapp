4b6a39a5e4279d330baafaf58b45223c6bf7a3a5
André Gabriel <andreon098@gmail.com>
2025-10-20 09:37:54 -0300

    fix: update mail module to use async configuration with ConfigService

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,4 +1,6 @@
 import * as path from 'node:path'
+import { ConfigModule } from '@app/config/config.module'
+import { ConfigService } from '@app/config/config.service'
 import { MailerModule } from '@nestjs-modules/mailer'
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter'
 import { Module } from '@nestjs/common'
@@ -6,26 +8,30 @@ import { MailService } from './mail.service'
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.EMAIL_HOST,
-        port: parseInt(process.env.EMAIL_PORT),
-        secure: false,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASSWORD,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.EMAIL_HOST,
+          port: configService.EMAIL_PORT,
+          secure: false,
+          auth: {
+            user: configService.EMAIL_USER,
+            pass: configService.EMAIL_PASSWORD,
+          },
         },
-      },
-      defaults: {
-        from: process.env.EMAIL_FROM,
-      },
-      template: {
-        dir: path.join(process.cwd(), 'dist', 'modules', 'mail', 'templates'),
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
+        defaults: {
+          from: configService.EMAIL_FROM,
         },
-      },
+        template: {
+          dir: path.join(process.cwd(), 'dist', 'modules', 'mail', 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
     }),
   ],
   providers: [MailService],
